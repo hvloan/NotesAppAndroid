9f431e6fde60b307918a6b5fddb6b3b85d17de61
hvloan <hvloan.work@gmail.com>
2022-11-17 20:38:16 +0700

    update layout create note activity

---
+++ app/src/main/java/vn/com/hvloan/notesapp/CreateNoteActivity.kt
@@ -0,0 +1,11 @@
+package vn.com.hvloan.notesapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CreateNoteActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_note)
+    }
+}
