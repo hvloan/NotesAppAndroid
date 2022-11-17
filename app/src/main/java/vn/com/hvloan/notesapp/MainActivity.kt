cb4facd2ce0f2180394615a3ed0b92e84c7c031e
hvloan <hvloan.work@gmail.com>
2022-11-17 22:23:07 +0700

    update main activity

--- app/src/main/java/vn/com/hvloan/notesapp/MainActivity.kt
+++ app/src/main/java/vn/com/hvloan/notesapp/MainActivity.kt
@@ -1,11 +1,33 @@
 package vn.com.hvloan.notesapp
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
+    val REQUEST_CODE_ADD_NOTE = 1
+    lateinit var imageAddNoteMain: ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        initComponent()
+        actionComponent()
+    }
+
+    private fun actionComponent() {
+        imageAddNoteMain.setOnClickListener {
+            val intent = Intent(this, CreateNoteActivity::class.java)
+            startActivityForResult(intent, REQUEST_CODE_ADD_NOTE)
+        }
+    }
+
+    private fun initComponent() {
+        imageAddNoteMain = findViewById(R.id.imageAddNoteMain)
     }
 }
